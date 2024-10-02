e6345b7bb1ea01f2740925cdf3346e36a066460b
lovenios <2499857343@qq.com>
2024-10-02 11:42:49 +0800

    test: update

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert']],
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-max-length': [2, 'always', 72],
+    'body-max-length': [2, 'always', 72],
+    'footer-max-length': [2, 'always', 72],
+  },
+};
